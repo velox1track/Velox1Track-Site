0b918438ad8c43e427d8204a184aa347f7ad3caf
velox1track <velox1track@gmail.com>
2025-08-03 14:39:37 -0400

    Update App.jsx - delete content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,1 @@
-import React from 'react'
-import RouletteWheel from './components/RouletteWheel'
 
-function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
-      <div className="max-w-4xl mx-auto">
-        <RouletteWheel />
-      </div>
-    </div>
-  )
-}
-
-export default App 
